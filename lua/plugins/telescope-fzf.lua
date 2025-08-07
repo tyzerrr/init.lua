a7810c5d599a6d3a1acf7cc87730dd3bd28ccf13
Taichi Araki <araki130710@gmail.com>
2025-08-07 21:38:53 +0900

    feat: add new build policy to cmake minimum version

--- lua/plugins/telescope-fzf.lua
+++ lua/plugins/telescope-fzf.lua
@@ -1,4 +1,4 @@
 return {
 	"nvim-telescope/telescope-fzf-native.nvim",
-	build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+	build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --config Release",
 }
