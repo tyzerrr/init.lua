bd90c124036f05295ff3d0b4db6456877baf9020
Taichi Araki <araki130710@gmail.com>
2025-10-26 00:08:36 +0900

    feat: update trouble.nvim

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -6,37 +6,49 @@ return {
         use_diagnostic_signs = true,
     }, -- for default options, refer to the configuration section for custom setup.
     lazy = false,
-    cmd = "Trouble",
-    keys = {
-        {
-            "<leader>xx",
-            "<cmd>Trouble diagnostics toggle win.position=right<cr>",
-            desc = "Diagnostics (Trouble)",
-        },
-        {
-            "<leader>xX",
-            "<cmd><cmd>Trouble diagnostics toggle filter.buf=0 win.position=right<cr>",
-            desc = "Buffer Diagnostics (Trouble)",
-        },
-        {
-            "<leader>cs",
-            "<cmd>Trouble symbols toggle focus=false<cr>",
-            desc = "Symbols (Trouble)",
-        },
-        {
-            "<leader>cl",
-            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-            desc = "LSP Definitions / references / ... (Trouble)",
-        },
-        {
-            "<leader>xL",
-            "<cmd>Trouble loclist toggle<cr>",
-            desc = "Location List (Trouble)",
-        },
-        {
-            "<leader>xQ",
-            "<cmd>Trouble qflist toggle<cr>",
-            desc = "Quickfix List (Trouble)",
-        },
-    },
+    config = function()
+        ---@function modeOpts
+        ---@param mode string
+        ---@return table
+        local modeOpts = function(mode)
+            return {
+                mode = mode,
+                win = {
+                    type = "split",
+                    position = "right",
+                }
+            }
+        end
+
+        require("trouble").setup()
+
+        local diagnostics = modeOpts("diagnostics")
+        local quickfix = modeOpts("quickfix")
+
+        --diagnostics
+        vim.keymap.set("n", "<leader>xx", function()
+            require("trouble").toggle(diagnostics)
+        end)
+
+        vim.keymap.set("n", "<leader>xn", function()
+            require("trouble").next(diagnostics, {})
+        end)
+
+
+        vim.keymap.set("n", "<leader>xp", function()
+            require("trouble").prev(diagnostics, {})
+        end)
+
+        ---quickfix
+        vim.keymap.set("n", "<leader>qf", function()
+            require("trouble").toggle(quickfix)
+        end)
+        vim.keymap.set("n", "<leader>qn", function()
+            require("trouble").next(quickfix, {})
+        end)
+
+        vim.keymap.set("n", "<leader>qp", function()
+            require("trouble").prev(quickfix, {})
+        end)
+    end
 }
